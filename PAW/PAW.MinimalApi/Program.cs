3adde11215a1f3bcebbff413e29dd46cdbcf0a7e
AndresALPD <andresalpizar03@gmail.com>
2025-06-18 18:08:46 -0600

    Pending work

--- PAW/PAW.MinimalApi/Program.cs
+++ PAW/PAW.MinimalApi/Program.cs
@@ -1,13 +1,18 @@
+using PAW.MinimalApi.Factory;
+using PAW.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Registramos el Factory en el contenedor de dependencias
+builder.Services.AddScoped<ICatalogFactory, CatalogFactory>();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +20,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Endpoint de prueba para crear entity
+app.MapGet("/create-entity", (ICatalogFactory factory) =>
+{
+    var entity = factory.CreateEntity<Catalog>("AndresCatalog");
+    return Results.Ok(entity);
+});
 
 app.UseHttpsRedirection();
 
